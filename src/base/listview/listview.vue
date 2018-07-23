ac661d34416d3c892bca4d6c6f4f6f61c48b4617
tarogege <671309751@qq.com>
2018-07-23 18:14:41 +0800

    new file:   src/base/listview/listview.vue 	modified:   src/components/singer/singer.vue

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="listview">
+    <div v-for="(typelist,index) in singerList" :key="index">
+      <div class="title">{{typelist.title}}</div>
+      <ul class="singerlist">
+        <li v-for="(singer,singerindex) in typelist.items" :key="singerindex" class="singeritem">
+          <div class="avatar">头像</div>
+          <div class="name">{{singer.name}}</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    singerList: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.listview
+  .title
+    background-color #7e8c8d
+  .singerlist
+    .avatar
+      font-size: $font-size-small
+      color: $color-text-l
+      background: $color-highlight-background
+    .singeritem
+      padding 20px 10px
+      border-bottom 1px solid #7e8c8d
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,24 +1,84 @@
 <template>
-<div class="singer">singer</div>
+<div class="singer">
+  <listview :singerList="singerList"></listview>
+</div>
 </template>
 
 <script type="text/ecmascript-6">
 import { getSingerList } from '../../api/singers'
+import Listview from '../../base/listview/listview'
 
+const HOT_NAME = '热门'
+const HOT_NUM = 10
 export default {
   data () {
     return {
       singerList: []
     }
   },
+  components: {
+    listview: Listview
+  },
   methods: {
     _getSingerList() {
       getSingerList().then((res) => {
-        console.log(res)
         if (res.code === 0) {
           this.singerList = res.data.list
+          console.log(this.singerList)
+          this.singerList = this._normalizeSingerList(this.singerList)
         }
       })
+    },
+    _normalizeSingerList(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_NUM) {
+          map.hot.items.push({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          })
+        }
+
+        let key = item.Findex
+        if (!map[key] && key !== '9') {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        if (key === '9') {
+          key = '#'
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        })
+      })
+
+      //  对map进行排序
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
     }
   },
   created() {
